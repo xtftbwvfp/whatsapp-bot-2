c213ba3846ce46278fa8272c364482cb9b462c57
Javier Calero <javier@evercam.io>
2020-06-20 15:07:28 +0100

    get session from s3

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fs = require("fs")
 const fetch = require("node-fetch")
 const { default: PQueue } = require("p-queue")
 const wakeDyno = require("woke-dyno")
+var AWS = require("aws-sdk")
 
 var utils = require("./utils")
 const server = require("./server")
@@ -20,6 +21,45 @@ const queue = new PQueue({
   autoStart: false,
 })
 
+var s3 = new AWS.S3()
+s3.getObject(
+  { Bucket: process.env.S3_BUCKET_NAME, Key: "session1.data.json" },
+  function (error, data) {
+    if (error != null) {
+      console.log("Failed to retrieve an object: " + error)
+    } else {
+      console.log("Loaded " + data.ContentLength + " bytes")
+      fs.promises.mkdir("./static", { recursive: true }).then(() =>
+        fs.promises.writeFile(
+          "./static/session1.data.json",
+          JSON.stringify(JSON.parse(data.Body)),
+          function (err) {
+            if (err) return console.log(err)
+          }
+        )
+      )
+    }
+    wa.create({
+      sessionId: "session1",
+      sessionDataPath: "static",
+      headless: process.env.NODE_ENV === "development" ? false : true,
+      throwErrorOnTosBlock: true,
+      restartOnCrash: start,
+      killProcessOnBrowserClose: false,
+      autoRefresh: false,
+      qrRefreshS: 15,
+      qrTimeout: 40,
+      cacheEnabled: false,
+      customUserAgent:
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3882.0 Safari/537.36",
+    })
+      .then(async (client) => await start(client))
+      .catch((e) => {
+        console.log("Error", e.message)
+      })
+  }
+)
+
 server.listen(PORT, () => {
   wakeDyno(process.env.DYNO_URL).start()
 })
@@ -30,26 +70,6 @@ ON_DEATH(async function (signal, err) {
   console.log(err)
   if (globalClient) await globalClient.kill()
 })
-
-wa.create({
-  sessionId: "session1",
-  sessionDataPath: "static",
-  headless: process.env.NODE_ENV === "development" ? false : true,
-  throwErrorOnTosBlock: true,
-  restartOnCrash: start,
-  killProcessOnBrowserClose: false,
-  autoRefresh: false,
-  qrRefreshS: 15,
-  qrTimeout: 40,
-  cacheEnabled: false,
-  customUserAgent:
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3882.0 Safari/537.36",
-})
-  .then(async (client) => await start(client))
-  .catch((e) => {
-    console.log("Error", e.message)
-    // process.exit();
-  })
 
 wa.ev.on("qr.**", async (qrcode, sessionId) => {
   const imageBuffer = Buffer.from(
@@ -63,9 +83,15 @@ wa.ev.on("qr.**", async (qrcode, sessionId) => {
 })
 
 wa.ev.on("sessionData.**", async (sessionData, sessionId) => {
-  console.log("\n----------")
-  console.log("sessionData", sessionId, sessionData)
-  console.log("----------")
+  const params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: `${sessionId ? "_" + sessionId : ""}.data.json`,
+    Body: JSON.stringify(sessionData, null, 2),
+  }
+  s3.upload(params, function (s3Err, data) {
+    if (s3Err) throw s3Err
+    console.log(`File uploaded successfully at ${data.Location}`)
+  })
 })
 
 async function start(client) {
